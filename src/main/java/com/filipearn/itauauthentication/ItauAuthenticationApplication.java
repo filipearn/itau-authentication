3bbd8d91d204a71ae5aaeb50ab558ba5611028a6
filipearn <filipearn@yahoo.com.br>
2024-05-09 23:41:44 -0300

    feat: criando estrutura inicial de projeto

---
+++ src/main/java/com/filipearn/itauauthentication/ItauAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.filipearn.itauauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filipearn/itauauthentication/ItauAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipearn.itauauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
