fac616bd51bdebafa6540aede4d3118f96d5f310
filipearn <filipearn@yahoo.com.br>
2024-05-10 19:16:51 -0300

    fix: consertando erro em validacao de primos para 0 e 1 e aumentando a eficiencia

--- src/main/java/com/filipearn/itauauthentication/app/usecases/SeedClaimValidationStrategy.java
+++ src/main/java/com/filipearn/itauauthentication/app/usecases/SeedClaimValidationStrategy.java
@@ -28,7 +28,16 @@ public class SeedClaimValidationStrategy extends AbstractJWTValidationStrategy{
     }
 
     private boolean isPrime(long number){
-        for (int i = 2; i <= Math.sqrt(number); i++) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number == 2) {
+            return true;
+        }
+        if (number % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(number); i+=2) {
             if (number % i == 0) {
                 return false;
             }
